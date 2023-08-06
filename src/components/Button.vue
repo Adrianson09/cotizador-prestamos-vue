40950533a0d9dd28643e9a90244f31ab6937276e
Adrian Guerrero <adrianson@outlook.com>
2023-08-05 21:30:06 -0600

    emitir eventos entre componentes, para pasar parametros

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -2,7 +2,7 @@
     // const props = defineProps(['operador'])
     const props = defineProps({
         operador: String,
-        fn: Function
+        
     })
 </script>
 
@@ -12,6 +12,6 @@
         class="h-10 w-10 flex items-center justify-center font-bold bg-lime-500
         rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2
         hover:ring-lime-500 text-white text-2xl"
-        @click="fn"
+        @click="$emit('fn')"
         >{{operador}}</button>
 </template>
