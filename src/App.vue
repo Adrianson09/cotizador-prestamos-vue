c7b50f37e8f203030a2f67b3b1de4900d3a5d7a8
Adrian Guerrero <adrianson@outlook.com>
2023-08-05 22:14:11 -0600

    calcular total

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import {ref, computed} from 'vue';
 import Header from './components/Header.vue';
 import Button from './components/Button.vue';
+import {totalPagar} from './helpers/index.js'
 
 const cantidad = ref(100000);
-
-const MIN= 0
-const MAX= 200000
-const STEP= 5000
+const meses = ref(6);
+const total = ref(totalPagar(cantidad.value, meses.value));
+const MIN= 0;
+const MAX= 200000;
+const STEP= 5000;
 
 const formatearDinero = computed(() => {
   const formatter = new Intl.NumberFormat('es-CR',{
@@ -59,6 +61,27 @@ const handleChangeIncremento = () => {
        
         />
         <p class="text-center my-10 text-5xl font-extrabold text-green-800">{{ formatearDinero }}</p>
+        <h2 class="text-2xl font-extrabold text-gray-500 text-center">
+          Elige un <span class="text-green-800">Plazo</span> a Pagar
+        </h2>
+
+        <select class="w-full p-2 bg-white border border-gray-300 rounded-lg text-center
+        text-xl font-bold text-gray-500 mt-5"
+        :value="meses"
+        v-model.number="meses">
+          <option value="6">6 Meses</option>
+          <option value="12">12 Meses</option>
+          <option value="24">24 Meses</option>
+        </select>
+      </div>
+      <div class="my-5 space-y-3 bg-gray-50 p-5">
+        <h2 class="text-2xl font-extrabold text-gray-500 text-center">
+          Resumen <span class="text-green-800">de Pagos</span>
+        </h2>
+        <p class="text-xl text-gray-500 text-center font-bold">{{ meses }} Meses</p>
+        <p class="text-xl text-gray-500 text-center font-bold">Total a pagar: {{ formatearDinero( total ) }}</p>
+        <p class="text-xl text-gray-500 text-center font-bold">Mensualidades de: </p>
+
       </div>
   </div>
  
---
+++ src/helpers/index.js
@@ -0,0 +1,29 @@
+const totalPagar = (cantidad, plazo) => {
+let total;
+// Mientras mayor es la cantidad, menor es el interes
+    if(cantidad < 50000){
+        total = cantidad * 1.5;
+    }else if(cantidad >= 50000 && cantidad < 100000){
+        total = cantidad * 1.4;
+
+    }else if(cantidad >= 100000 && cantidad < 150000) {
+        total = cantidad * 1.3;
+
+    }else{
+        total = cantidad * 1.2;
+
+    }
+    //  + Plazo, + interes
+
+    if (plazo === 6) {
+        total *= 1.1
+    }else if(plazo === 12){
+        total *= 1.2
+    }else {
+        total *= 1.3
+    }
+}
+
+export {
+    totalPagar
+}
