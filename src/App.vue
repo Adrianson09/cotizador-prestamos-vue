c676da298ed70ef6b0ebc2e758b950c1da2d7e52
Adrian Guerrero <adrianson@outlook.com>
2023-08-05 20:36:45 -0600

    entrando a V-model

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,36 @@
 <script setup>
+import {ref} from 'vue';
+import Header from './components/Header.vue';
+
+const cantidad = ref(100000);
+
+const MIN= 0
+const MAX= 200000
+const STEP= 5000
+
+function handleChange(e){
+  cantidad.value = +e.target.value;
+}
 
 </script>
 
 <template>
-  <h1>Hola Mundo!</h1>
+  <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
+      <Header/>
+      <div class="my-5">
+        <input 
+        type="range"
+        class="w-full bg-gray-200 accent-lime-500 hover:accent-lime-600"
+        :min="MIN"
+        :max="MAX"
+        :step="STEP"
+        :value="cantidad"
+        @input="handleChange"
+       
+        />
+        {{ cantidad }}
+      </div>
+  </div>
  
 </template>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+
+    <h1 class="text-4xl font-extrabold text-gray-500 text-center">
+        ¿Cuánto <span class="text-green-800">Dinero</span> necesitas?
+    </h1>  
+    
+</template>
